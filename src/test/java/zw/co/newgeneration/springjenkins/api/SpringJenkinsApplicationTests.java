e99cd54b393c75b18540ff5dcb520cf3e1627d99
dennisnyoni <dennisnyoni@rocketmail.com>
2023-02-01 14:19:07 -0600

    first commit

---
+++ src/test/java/zw/co/newgeneration/springjenkins/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package zw.co.newgeneration.springjenkins.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+    public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplicationTest.class);
+
+    @Test
+    void contextLoads() {
+        logger.info("Test case executing ... ");
+        assertEquals(true, ture);
+    }
+
+}
